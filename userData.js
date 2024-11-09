0a4da1469b66090d4a14b1335149d78ef9ab637a
nerellamanoj <nerellamanojkumar@gmail.com>
2024-11-09 15:45:35 +0530

    initial commit

---
+++ userData.js
@@ -0,0 +1,76 @@
+// userData.js
+
+const users = [
+    {
+      name: "Manoj Kumar",
+      position: "Full Stack Developer",
+      company: "FreeLancer",
+      image: "./images/manoj.jpeg",
+      description:
+        "Recognized for strong analytical skills, a collaborative approach, and a commitment to continuous improvement. Enthusiastic about leveraging technical expertise to contribute to innovative projects and drive success",
+      socialLinks: [
+        { icon: "🌐", url: "#" },
+        { icon: "✉️", url: "#" },
+        { icon: "🔗", url: "#" },
+      ],
+    },
+    {
+      name: "sharath ",
+      position: "Chief Engagement Officer",
+      company: "Acquia",
+      image:
+        "https://s3-alpha-sig.figma.com/img/9de3/b8aa/4e6fd86d00bc645311899b3fbcca52d5?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=i-2az8c~TQ4GQTgMdGGA~OiZRcxQqNliOmg1Nrh3~q5mCbkyAXoOFrZXz3rtpBmhoLH2MEPcD8uyzrysiBixOxyySVYRLqY2pdQYKhqW1Kibe4meVDaoZKzWOedglJPRuLQhW62vUhFnnDWDaxe74zfw1MfHfD-qLD4-v2FEWZy4Ww64U6DTEIPNSgwu2vhoCu0UzxocqOeUDrobUZuHnzPMc29WeTKpzHo~9NfifEoSciD~yZUa0s40o9Q~gM54g-UhG-46MlVop01DP9xSiLXP3RnQq6B3DimGuPflSgw9-5gSLX5Z94~AVOpkYwkxNCs-jiWwTnZfxmyz5X~qIw__",
+      description:
+        "lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nulla facilisi. lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nulla facilisi. lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nulla facilisi.",
+      socialLinks: [
+        { icon: "🌐", url: "#" },
+        { icon: "✉️", url: "#" },
+        { icon: "🔗", url: "#" },
+      ],
+    },
+    {
+      name: "chandrash",
+      position: "Chief Technical Developer",
+      company: "Pantheon",
+      image:
+        "https://s3-alpha-sig.figma.com/img/9134/0d19/81845f21043dd8f5192da248767c4eae?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=k803O02X0pXmvOoAJFVO6iTOVx5GGHL72bT8~YUINLU1Y6aGiPW2aRLhexGOSlbBUQ89XvZZIyecwmCJ5cV~hgYwOhN~oSLMER17lHrYCYNHAT2Jcn9BekVJbmj3zvnHHZVJR4KKR0tPZpDmCae~SI32EupBZK9ysXymdgpxj-WznEuPuu1xtWW3fyx7~ItSbU7hR86mpPpdqZ-Ds6PZH~4ZPPIkPip0oT7MehKrtQe-yXfJIoMZLu-bY9A02b~tUb2PtcZWKYBOElVVVcZRfkAG4MNx0B5RxvSxXsadVuMULmz3yebUvtY5qJrJy8gCIGCRpN35dN~qR3HpnuflCw__",
+      description:
+        "lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nulla facilisi. lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nulla facilisi. lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nulla facilisi.",
+      socialLinks: [
+        { icon: "🌐", url: "#" },
+        { icon: "✉️", url: "#" },
+        { icon: "🔗", url: "#" },
+      ],
+    },
+    {
+      name: "Mike",
+      position: "Chief Marketing Officer",
+      company: "Specbee",
+      image:
+        "https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      description:
+        "lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nulla facilisi. lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nulla facilisi. lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nulla facilisi.",
+      socialLinks: [
+        { icon: "🌐", url: "#" },
+        { icon: "✉️", url: "#" },
+        { icon: "🔗", url: "#" },
+      ],
+    },
+    {
+      name: "Don",
+      position: "Chief Marketing Officer",
+      company: "Specbee",
+      image:
+        "https://s3-alpha-sig.figma.com/img/aeba/3e61/75255eef4aafbeaf378e32d61894f628?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=THCVaU2hZrwyAHEhKFhbXoYU4qg1f7~FK7hrPzcA1iCzXSUKV88oyZd5eSNfxrMuuAW4CK4XbwO3NCLAk2QGzmpSTcqyvuz7honPyZsNEAklbn-Gq6x06PEkqzZWPaa2RBro6TBvLC9SUsP3S9LXnzg53jVNaJ5yg2YOBbUeLp3qzOF0zGCkKdBDFJ0EnvHqqqLmAEvMrhcxNBTbyWi2F0I7yk15nb5obNZYbGGz0Tr7z7U-tWrbHtmPC3ld7AFB~IBamtN9nQQ5GpER0agWBD0ZrbpLBfMLoCxhhIZQZeigQzKxqp9d~g8aWucIgPAZh8pDE-7IxdZjQ~CjaHDmEQ__",
+      description:
+        "lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nulla facilisi. lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nulla facilisi. lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nulla facilisi.",
+      socialLinks: [
+        { icon: "🌐", url: "#" },
+        { icon: "✉️", url: "#" },
+        { icon: "🔗", url: "#" },
+      ],
+    },
+  ];
+  
+  //
+  
